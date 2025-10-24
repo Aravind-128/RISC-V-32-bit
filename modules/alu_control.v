819a296476c648906ac3c6713b55c44b8536f729
Aravind-128 <chongaliaravind77@gmail.com>
2025-10-24 19:21:51 +0530

    Update alu_control.v

--- modules/alu_control.v
+++ modules/alu_control.v
@@ -12,25 +12,17 @@ module alu_con(
     reg [3:0] ctrl;
     always@(*) 
         casez({alu_op,fun3,op,fun7})
-        7'b00_zzz_z_z : ctrl= 4'b000 ;  // for lw,sw
-        7'b01_zzz_z_z : ctrl = 4'b001;  // for branch
+          7'b10_000_1_0 : ctrl=4'b0000;   // addd
+          7'b10_000_1_1 : ctrl=4'b0001;     // sub
+          7'b10_111_1_0 : ctrl=4'b0010;     // AND
+          7'b10_110_1_0 : ctrl=4'b0011;     // OR
+          7'b10_001_1_0 : ctrl=4'b0100;     // sll
+          7'b10_010_1_0 : ctrl=4'b0101;     //slt
+          7'b10_101_1_0 : ctrl=4'b0110;     // srl
+          7'b10_101_1_1 : ctrl=4'b0111;     // sra
+          7'b10_100_1_0 : ctrl=4'b1000;     // XOR
         
-        /////////////////////////////////////////////////////////////////////////////////
-        
-        7'b10_000_0_0,7'b10_000_0_1,7'b10_000_1_0 : ctrl=4'b000 ;  // for add
-        7'b10_000_1_1:ctrl = 4'b001 ;   //for subtract
-        7'b10_111_z_z:ctrl = 4'b010 ;   //for and
-        7'b10_110_z_z:ctrl = 4'b011 ;   //for or
-        7'b10_001_z_0:ctrl = 4'b100 ;   // for shift logical left
-        7'b10_010_z_0:ctrl = 4'b101 ;   //for set less than
-        7'b10_101_z_0:ctrl = 4'b110;    // for shift logical right
-        7'b10_101_1_1:ctrl = 4'b111;    // for shift right arithmetic
-        7'b10_100_z_0:ctrl = 4'b1000;   // for xor 
-     
-        
-       /////////////////////////////////////////////////////////////////////////////////////
-        
-        endcase
-        
+         endcase
+      
         assign alu_ctrl = ctrl ;
 endmodule
