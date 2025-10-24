a2c9e1fc2ed6647b2c6db0331783348a46f24bb5
Aravind-128 <chongaliaravind77@gmail.com>
2025-10-24 15:20:33 +0530

    Implement program counter module

--- modules/program_counter.v
+++ modules/program_counter.v
@@ -1,1 +1,19 @@
 
+
+
+`timescale 1ns / 1ps
+
+module program_counter(
+    input clk,
+    input rst,
+    input [31:0] pc_in,   // input of program counter 
+ 
+    output reg [31:0] pc_out    );
+
+always@(posedge clk)
+begin
+     if(rst) pc_out<=32'd0;
+     else pc_out<=pc_in;        
+end
+endmodule
+
